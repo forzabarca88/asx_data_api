13b902f533bcb3bf4f29ec6ff68c43c34588f082
Justin D'Cruze <forzabarca88@gmail.com>
2019-12-07 17:07:11 +1100

    refactor into 2 files, remove pre-cache step

--- app.py
+++ app.py
@@ -1,44 +1,12 @@
 from flask import Flask, jsonify
-import requests
-import csv
-import multiprocessing
-import os
-import datetime
-import json
+import functions
+import threading
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    return app
 
-def precache_data():
-    url = 'https://www.asx.com.au/asx/research/ASXListedCompanies.csv'
-    data = requests.get(url, allow_redirects=True).content
-    with open('asx.csv', 'wb') as f:
-        f.write(data)
-    with open('asx.csv', 'r', newline='') as f:
-        csv_data = list(csv.reader(f))
-    csv_data = [line[1] for line in csv_data[3:]]
-    try:
-        os.mkdir('data')
-    except:
-        pass
-    with multiprocessing.Pool() as pool:
-        pool.map(get_data, csv_data)
-
-def get_data(company):
-    filename = os.path.join('data', '{}_{}'.format(company, str(datetime.date.today())))
-    if os.path.exists(filename):
-        print('Skipping {}...'.format(filename))
-        return True
-    data = requests.get('https://www.asx.com.au/asx/1/company/{}?fields=primary_share'.format(company))
-    json_data = data.json()
-    if 'code' in json_data and 'primary_share' in json_data: 
-        with open(filename, 'wb') as f:
-            f.write(data.content)
-        print(company)
-        return True
-    else:
-        print('ERROR while processing {}'.format(company))
-        print(json_data)
-        return False
+app = create_app()
 
 @app.route('/')
 def index():
@@ -52,18 +20,11 @@ def index():
 
 @app.route('/company/<id>', methods=['GET']) 
 def get_company_data(id):
-    filename = os.path.join('data', '{}_{}'.format(id, str(datetime.date.today())))
-    try:
-        with open(filename, 'r') as f:
-            data = json.load(f)
-        if 'code' in data and 'primary_share' in data:
-            return jsonify(data), 200
-        else:
-            raise requests.RequestException('Abnormal response: {}'.format(data))
-    except Exception as ex:
-        return jsonify('{}'.format(ex)), 404
+    functions.get_data(id)
+    filename = functions.generate_filename(id)
+    data, code = functions.open_file_and_return_data(filename)
+    return jsonify(data), code
 
 if __name__ == "__main__":
-    precache_data()
     app.run(debug=True)
 
